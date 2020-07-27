d0800535011a83816ada9bdb0884148052fa3d86
Trillium <spiteless@gmail.com>
2020-07-27 13:14:11 -0700

    Completed step 6

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,6 +1,14 @@
 const products = require('../products.json')
 
 const getProducts = (req, res) => {
+
+    if (req.query.price) {
+        const items = products.filter( item => item.price >= Number(req.query.price))
+        res.status(200).send(items.filter( prod => {
+            return prod.price >= Number(req.query.price)
+        }))
+    }
+
     res.status(200).send(products);
 }
 
