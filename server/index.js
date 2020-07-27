e6ad9860de1d3c7b82622231551241fd2dcd5be1
Trillium <spiteless@gmail.com>
2020-07-27 12:51:46 -0700

    Completed step 3

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
-
+const products = require('../products.json')
 app = express()
 
 const port = 4041
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
+
+app.get(`/api/products`, (req, res) => {
+    res.status(200).send(products)
+})
+
+//this is a useless comment
